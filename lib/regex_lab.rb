3aa52bb9afa106e6e720443588f93c86c541364e
Andrew Caniglia <andrewcaniglia@gmail.com>
2020-12-29 04:15:45 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,10 +12,10 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/^[A-Z]+[.?!]$/)
+  text.match?(/^[A-Z]+[.?!]$/)
 
 end
 
 def valid_phone_number?(phone)
-
+  text.match?
 end
